50c5a991a67c81622266a7e657c6e9f8591a33b0
bonvi83 <bonvi83@gmail.com>
2024-03-20 19:34:58 +0100

    error stampa in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
-@section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel Migration Seeder</h1>
-      <h3>Tabella treni</h3>
-      <div>
-        qui visualizzo la mia tabella
-      </div>
-    </div>
+  <section class="container mt-5">
+  
+    @forelse($trains as $train)
+      <p>
+        <strong>Type</strong>: {{ $train->Azienda }} <br>
+        {{-- <strong>Stazione di partenza</strong>: {{ $train->'Stazione di partenza' }} <br> --}}
+      </p>
+      <hr>
   </section>
 @endsection
+
+
