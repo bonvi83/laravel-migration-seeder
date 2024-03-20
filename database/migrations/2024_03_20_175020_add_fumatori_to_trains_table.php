9567fa1b98fe5c7cf8d049ef92a6f1d94ffeda1f
bonvi83 <bonvi83@gmail.com>
2024-03-20 19:03:52 +0100

    add nuovo campo

---
+++ database/migrations/2024_03_20_175020_add_fumatori_to_trains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // aggiungo un campo fumatori alla mia tabella trains e la sposto dove voglio (non alla fine della tabella)
+        Schema::table('trains', function (Blueprint $table) {
+            $table->boolean('fumatori')->after('Posto a sedere');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
+    }
+};
